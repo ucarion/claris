29ba5282d2418ef25ca90038de39eb166afd9383
Ulysse Carion <ulysse@ulysse.io>
2015-01-09 13:10:35 -0800

    Make song addition work for Spotify.

--- music.rb
+++ music.rb
@@ -2,10 +2,13 @@ require 'shellwords'
 
 module Music
   def self.queue_song(song_name)
-    song_id = `mpc search any #{song_name.shellescape}`.split("\n").first
-    `mpc clear`
+    results = `mpc search any #{song_name.shellescape}`.split("\n")
+    song_id = results.find { |entry| entry.include?('track') }
+
     `mpc add "#{song_id.shellescape}"`
-    `mpc playlist --format="%title%"`
+
+    # Get the name of the song just added
+    `mpc playlist`.split("\n").last
   end
 
   def self.unpause
