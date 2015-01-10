a5f23875ad462ecabf96e27a662beddf41f2d501
Ulysse Carion <ulysse@ulysse.io>
2015-01-10 15:14:41 -0800

    Use periodic recording rather than keyword spotting.

--- voice_activation.rb
+++ voice_activation.rb
@@ -1,12 +1,20 @@
+require_relative 'util'
+
 require 'pocketsphinx-ruby'
 
 module VoiceActivation
   def self.listen_for_keyword(keyword, &block)
     Pocketsphinx.disable_logging
 
-    configuration = Pocketsphinx::Configuration::KeywordSpotting.new(keyword)
-    recognizer = Pocketsphinx::LiveSpeechRecognizer.new(configuration)
+    message_path = Util.record_message
+    recognizer = Pocketsphinx::AudioFileSpeechRecognizer.new
 
-    recognizer.recognize(&block)
+    recognizer.recognize(message_path) do |speech|
+      if speech == keyword
+        block.call
+      else
+        listen_for_keyword(keyword, &block)
+      end
+    end
   end
 end
