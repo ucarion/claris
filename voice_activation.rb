de703cd603fc07a3e91a0f87025436387c117d8f
Ulysse Carion <ulysse@ulysse.io>
2015-01-12 11:30:56 -0800

    Switch back to keyword spotting.

--- voice_activation.rb
+++ voice_activation.rb
@@ -6,15 +6,7 @@ module VoiceActivation
   def self.listen_for_keyword(keyword, &block)
     Pocketsphinx.disable_logging
 
-    message_path = Util.record_message
-    recognizer = Pocketsphinx::AudioFileSpeechRecognizer.new
-
-    recognizer.recognize(message_path) do |speech|
-      if speech == keyword
-        block.call
-      else
-        listen_for_keyword(keyword, &block)
-      end
-    end
+    config = Pocketsphinx::Configuration::KeywordSpotting.new(keyword)
+    Pocketsphinx::LiveSpeechRecognizer.new(config).recognize(&block)
   end
 end
